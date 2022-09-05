4c68d97f62667481846089af02b3212ed07296d7
Ho <Ho@LAPTOP-NNFLJ359>
2022-09-05 13:51:58 +0900

    초기 커밋.

---
+++ MVC_ASPNETCORE/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ASPNETCORE.Models;
+using System.Diagnostics;
+
+namespace MVC_ASPNETCORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var firstUser = new User();
+            //firstUser.UserNo = 1;
+            //firstUser.UserName = "홍길동";
+
+            var HongUser = new User
+            {
+                UserNo = 1,
+                UserName = "홍길동"
+            };
+
+            //return View(HongUser);
+            //ViewBag.User = HongUser;
+
+            ViewData["User"] = HongUser;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_ASPNETCORE/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_ASPNETCORE.Models
+{
+    public class User
+    {
+        public int UserNo { get; set; } //사용자 번호
+        public string UserName { get; set; } // 사용자 이름
+
+    }
+}
